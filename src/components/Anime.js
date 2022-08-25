a7c2f68c124134f6d5034ec1f394666392cc7cb1
Esgrid Sikahall <esikahall@gmail.com>
2022-08-25 18:06:50 +0100

    adding commented hard coding of Featured Film and warning comment as a question

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,28 +1,34 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-function Anime (){
+function Anime() {
 
     const [anime, setAnime] = useState([])
 
     const getAnime = async () => {
-
         const result = await fetch('https://ghibliapi.herokuapp.com/films')
         const data = await result.json()
         setAnime(data)
         console.log(anime)
     }
 
+
     useEffect(() => {
         getAnime()
     }, [])
 
-    return(
+    return (
         <div>
             <h1>Anime Films</h1>
             <h2>Featured Film of the Month</h2>
 
+            {/* if one uncomments lines 26-28 and one reloads the page the app breaks - why? */}
+
+            {/* <h3>{anime[0].title}</h3>
+            <h4>{anime[0].release_date}</h4>
+            <p>{anime[0].description}</p> */}
+
             {
-                anime.map( film => {
+                anime.map(film => {
                     return <>
                         <h3>{film.title}</h3>
                         <p>{film.description}</p>
